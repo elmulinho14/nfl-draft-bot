1fd7dfa64f96095105e5ed88bfce27be35f5add5
Aleksandar Ilic <aleksandar.ilic@drillinginfo.com>
2025-04-19 14:27:58 +0200

    Added serving static build files

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const passport = require('passport');
 const DiscordStrategy = require('passport-discord').Strategy;
 const cors = require('cors');
 require('dotenv').config();
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -27,6 +28,9 @@ app.use(session({
 // Initialize Passport
 app.use(passport.initialize());
 app.use(passport.session());
+
+// Serve static files from the React app build directory
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 // Discord Strategy
 passport.use(new DiscordStrategy({
@@ -76,6 +80,12 @@ app.get('/api/logout', (req, res) => {
     if (err) { return next(err); }
     res.redirect('http://localhost:3000');
   });
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 // Start server
